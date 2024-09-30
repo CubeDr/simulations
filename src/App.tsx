23ede09203a63fb5096eba87bf18eb1c0693b4b9
Hyuni Kim <spaceship00@naver.com>
2024-09-30 01:28:51 +0000

    Add page header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import './App.css';
+import styles from './App.module.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.App}>
+      <header className={styles.Header}>
+        <h1 className={styles.Title}>Simulations</h1>
+        <a href="https://www.hyuni.dev" className={styles.Blog}>Blog</a>
+      </header>
     </div>
   );
 }
