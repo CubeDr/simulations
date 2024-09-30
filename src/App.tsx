b884d9c177856fbc16cec30ab0eaac9de55c0f56
Hyuni Kim <spaceship00@naver.com>
2024-09-30 01:50:07 +0000

    Add default routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import styles from './App.module.css';
+import GameOfLife from './game-of-life/GameOfLife';
 
 function App() {
   return (
-    <div className={styles.App}>
-      <header className={styles.Header}>
-        <h1 className={styles.Title}>Simulations</h1>
-        <a href="https://www.hyuni.dev" className={styles.Blog}>Blog</a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className={styles.App}>
+        <header className={styles.Header}>
+          <h1><a className={styles.Title} href="/">Simulations</a></h1>
+          <a href="https://www.hyuni.dev">Blog</a>
+        </header>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Link to="game-of-life">Conway's Game Of Life</Link>} />
+            <Route path="game-of-life" element={<GameOfLife />} />
+          </Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/game-of-life/GameOfLife.tsx
@@ -0,0 +1,3 @@
+export default function GameOfLife() {
+    return <>Game Of Life</>;
+}
