19b68db4b86b6bc737e733539fbcb4ed2db4bf7d
Hyuni Kim <spaceship00@naver.com>
2024-10-01 16:07:10 +0000

    Move with wheel

--- src/canvas/InteracionDetector.tsx
+++ src/canvas/InteracionDetector.tsx
@@ -13,21 +13,26 @@ interface Props {
 export default function InteractionDetector({ children, onClick, onRightClick, onHover, onMove, onZoom }: PropsWithChildren<Props>) {
   const containerRef = useRef<HTMLDivElement>(null);
 
-  const isDownRef = useRef(false);
+  const isLeftDownRef = useRef(false);
+  const isWheelDownRef = useRef(false);
   const isDraggingRef = useRef(false);
   const prevX = useRef(0);
   const prevY = useRef(0);
   const prevDistanceRef = useRef(0);
 
   /* ==== Down ===== */
-  const down = useCallback((x: number, y: number) => {
-    isDownRef.current = true;
+  const down = useCallback((x: number, y: number, button: number = 0) => {
+    if (button === 0) {
+      isLeftDownRef.current = true;
+    } else if (button === 1) {
+      isWheelDownRef.current = true;
+    }
     prevX.current = x;
     prevY.current = y;
   }, []);
 
   const onMouseDown = useCallback((e: MouseEvent) => {
-    down(e.clientX, e.clientY);
+    down(e.clientX, e.clientY, e.button);
   }, [down]);
 
   const onTouchDown = useCallback((e: React.TouchEvent) => {
@@ -49,7 +54,9 @@ export default function InteractionDetector({ children, onClick, onRightClick, o
 
   /* ===== Up ===== */
   const onMouseUp = useCallback((e: MouseEvent) => {
-    if (!isDraggingRef.current) {
+    const dx = e.clientX - prevX.current;
+    const dy = e.clientY - prevY.current;
+    if (dx * dx + dy * dy < MOVE_DRAG_THRESHOLD * MOVE_DRAG_THRESHOLD) {
       switch (e.button) {
         case 0:  // Left click
           onClick(x(e.clientX), y(e.clientY));
@@ -59,8 +66,13 @@ export default function InteractionDetector({ children, onClick, onRightClick, o
           break;
       }
     }
+
     isDraggingRef.current = false;
-    isDownRef.current = false;
+    if (e.button === 0) {
+      isLeftDownRef.current = false;
+    } else if (e.button === 1) {
+      isWheelDownRef.current = false;
+    }
     prevDistanceRef.current = 0;
   }, [onClick, onRightClick]);
 
@@ -80,7 +92,7 @@ export default function InteractionDetector({ children, onClick, onRightClick, o
   }, [onMove]);
 
   const onMouseMove = useCallback((e: MouseEvent) => {
-    if (isDownRef.current) {
+    if (isWheelDownRef.current) {
       move(e.clientX, e.clientY);
     } else {
       onHover(x(e.clientX), y(e.clientY));
@@ -92,7 +104,7 @@ export default function InteractionDetector({ children, onClick, onRightClick, o
       e.preventDefault();
 
       if (e.touches.length === 1) {
-        move(e.touches[0].clientX, e.touches[0].clientY);
+        // Drag
       } else if (e.touches.length === 2) {
         const centerX = (e.touches[0].clientX + e.touches[1].clientX) / 2;
         const centerY = (e.touches[0].clientY + e.touches[1].clientY) / 2;
